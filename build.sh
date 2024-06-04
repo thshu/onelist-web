6f6e97e0da9a2cde11835bd613eff52bd6bb1dfe
书小白 <2513002960@qq.com>
2024-06-04 17:25:14 +0800

    更新yarn.lock文件 添加build.sh

---
+++ build.sh
@@ -0,0 +1,15 @@
+MakeRelease(){
+    cd build
+    mkdir compress
+    tar -czvf compress/dist.tar.gz ../dist
+    rm -fr ../dist
+    cd compress
+    find . -type f -print0 | xargs -0 md5sum >md5.txt
+    cat md5.txt
+    cd ../..
+}
+
+
+yarn install
+npm run build
+MakeRelease
