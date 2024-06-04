d8827c30425fde3afb66ce1791789fbaa7dcfb71
书小白 <2513002960@qq.com>
2024-06-04 17:32:36 +0800

    更新release.yml 更新build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 MakeRelease(){
-    cd build
     mkdir compress
-    tar -czvf compress/dist.tar.gz ../dist
-    rm -fr ../dist
+    tar -czvf compress/dist.tar.gz dist
+    rm -f dist
     cd compress
     find . -type f -print0 | xargs -0 md5sum >md5.txt
     cat md5.txt
